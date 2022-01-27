62ed36cf4b4103c1d0c91950d6647420c62244ec
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-01-27 03:48:05 -0600

    Adds Validation for Register Route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,14 @@
 import {Request, Response} from 'express';
+import {RegisterValidation} from "../validation/register.validation";
 
 export const RegisterHandler = (req: Request, res: Response) => {
-    res.send(req.body);
+    const body = req.body;
+    const {error} = RegisterValidation.validate(body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    if (body.password !== body.passwordConfirmation) {
+        return res.status(400).send({message: 'Passwords do not match'});
+    }
+    res.send(body);
 };
