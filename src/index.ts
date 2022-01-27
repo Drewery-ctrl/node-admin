7a28040818298a9087f03e7e71f8203d5e0fb5c5
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-01-27 17:03:38 -0600

    Connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import express, {Request, Response} from "express";
+import express, { } from "express";
 import cors from "cors";
 import {routes} from "./routes/routes";
+import {Connection, createConnection} from "typeorm";
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.use(express.json());
-app.use(cors({origin: "http://localhost:3000"}));
+createConnection({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "root",
+    "database": "admin",
+    "entities": [
+        "src/entity/*.ts"
+    ],
+    "synchronize": true,
+    "logging": false
+}).then((connection: Connection) => {
+    console.log("Database connected: ", connection.isConnected);
+    const app = express();
+    app.use(express.json());
+    app.use(cors({origin: "http://localhost:3000"}));
 
-routes(app);
+    routes(app);
 
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
